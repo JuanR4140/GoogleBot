9729e659d7cf1420b2b1cf5f09aa060cddb15411
Juan Reyna <juan.reynarosas@gcpsva.org>
2020-12-18 00:01:46 +0000

    0.5v which works, but 9/10 of the time returns errors

--- main.py
+++ main.py
@@ -1,32 +1,64 @@
-prefix = "g+"
-
 import os
-import discord
-from discord.ext import commands
-
 from dotenv import load_dotenv
-load_dotenv()
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+
+from discord.ext import commands
+import discord
+
+load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='g+')
 
-@client.event
+@bot.event
 async def on_ready():
-  print(f'{client.user} is connected to Discord!')
+  print(f'{bot.user.name} has connected to Discord!')
 
-async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content == prefix + "intro":
-    await message.channel.send("```Hello, I am GoogleBot!\nI was made to be able to help you surf the web just a tiny bit faster!\nGet started by requesting me to make a Google search (g+search how tall is mount everest?)```")
+@bot.command(name="intro", help="GoogleBot's introduction")
+async def intro(ctx):
+  await ctx.send("```Hello, I am GoogleBot!\nI was made to be able to help you surf the web just a tiny bit faster!\nGet started by requesting me to make a Google search (g+search how tall is mount everest?)```")
 
-  if message.content == prefix + "search":
-    embed=discord.Embed(title="Showing Results For ", color=0xff0000)
-    embed.add_field(name="result 1", value="result 1", inline=True)
-    embed.add_field(name="result 2", value="result 2", inline=True)
-    embed.add_field(name="result 3", value="result 3", inline=True)
+@bot.command(name="search", help="Main feature of GoogleBot. Allows you to search Google quickly")
+async def search(ctx, *args):
+  await ctx.send("```retrieving page data, please wait..```")
+  url = "https://www.google.com/search?q="
+  wsurl = url + '+'.join(args)
+  
+  driver.get(wsurl)
+  
+  title = driver.find_elements_by_class_name("ellip")
+  desc = driver.find_elements_by_class_name("aCOpRe")
+  #print(title1.text)
+  #print(desc1.text)
+
+  
+  #print(driver.page_source)
+
+  try:
+    embed=discord.Embed(title="Showing Results For '" + ' '.join(args) + "'", color=0x37ff00) #0x37ff00 = green; 0xff0000 = red;
+    embed.add_field(name=title[0].text, value=desc[0].text, inline=True)
+    embed.add_field(name=title[1].text, value=desc[1].text, inline=True)
+    embed.add_field(name=title[2].text, value=desc[2].text, inline=True)
     embed.set_footer(text="Search Results brought to you by JuanR4140#0242 :D")
-    await message.channel.send(embed=embed)
+  except:
+    embed=discord.Embed(title="Search Results failed to load", color=0xff0000)
+    embed.add_field(name="(IndexError | Error 1)", value="Something went wrong when accessing list to display")
+    embed.set_footer(text="That's all we know.. :/")
+    await ctx.send(embed=embed)
 
-client.run(TOKEN)
+  try:
+    await ctx.send(embed=embed)
+  except:
+    embed=discord.Embed(title="Search Results failed to load", color=0xff0000)
+    embed.add_field(name="(Embed=Embed | Error 2)", value="Something went wrong when messaging the embed")
+    embed.set_footer(text="That's all we know.. :/")
+    await ctx.send(embed=embed)
+  await ctx.send(url + '+'.join(args))
+  #await ctx.send('{} arguments: {}'.format(len(args), ' '.join(args)))
+bot.run(TOKEN)
